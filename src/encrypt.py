c62d49f6cf2ff0780b1495ec09c6b15f986514ab
Thiago Andrés García González <thiago.garcia@correo.ucu.edu.uy>
2025-10-29 18:48:10 -0300

    cambito

--- src/encrypt.py
+++ src/encrypt.py
@@ -1,6 +1,4 @@
 import bcrypt
-from typing import Tuple
-from datetime import datetime
 
 def hash_pwd(password: str, rounds=16) -> bytes:
     pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt(rounds))
